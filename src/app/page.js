c39fc41b5e4a6b39702646272b36d02454ba1e8f
hildirama <hildirama123@gmail.com>
2025-08-29 20:50:13 +0700

    setup n fetching API

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,30 @@
 import Animelist from "@/components/Animelist"
+import Link from "next/link"
 
 const Home = async () => {
 
   const response = await fetch(`${process.env.
-    NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+    NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
 
     const anime = await response.json()
     
 
   return (
     <div>
-      <h1>Paling Populer</h1>
+      <div className="flex justify-between items-center p-4">
+      <h1 className="text-2xl font-bold">Paling Populer</h1>
+      <Link href="/populer" className="md:text-xl text-md underline hover:text-indigo-500 transition-all">Lihat semua</Link>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {anime.data.map (data =>  {
-        console.log(data.title)
+      return (
+      <div key={data.mal_id} className="shadow-xl">
+      <Animelist title={data.title} images={data.images.webp.
+        image_url} id={data.mal_id}/>
+        </div>
+      )
       })}
-      <Animelist/>
+      </div>
     </div>
   )
 }
