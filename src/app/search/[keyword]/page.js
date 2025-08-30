7039d644dcbe6de5f7f7b84198f9360d16b2d23f
hildirama <hildirama123@gmail.com>
2025-08-30 10:44:51 +0700

    input search api

---
+++ src/app/search/[keyword]/page.js
@@ -0,0 +1,24 @@
+import Animelist from "@/components/Animelist"
+import Header from "@/components/Animelist/Header"
+
+const Page = async ({params}) => {
+
+    const {keyword} = params
+
+  const response = await fetch(`${process.env.
+    NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`)
+
+    const searchAnime = await response.json()
+    
+
+  return (
+    <>
+      <section>
+      <Header title={`pencarian untuk ${keyword}...`} />
+      <Animelist api={searchAnime}/>
+      </section>
+    </>
+  )
+}
+
+export default Page
