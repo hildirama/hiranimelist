43dd200002a25422c47be505ac99572fb2fc7e77
hildirama <hildirama123@gmail.com>
2025-08-31 20:22:59 +0700

    nested & random data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,32 @@
-import Animelist from "@/components/Animelist"
-import Header from "@/components/Animelist/Header"
-import { getAnimeResponse } from "./libs/api-libs"
+import Animelist from "@/components/Animelist";
+import Header from "@/components/Animelist/Header";
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-libs";
 
 const Page = async () => {
-    const topAnime = await getAnimeResponse("top/anime", "limit=8")
+  const topAnime = await getAnimeResponse("top/anime", "limit=8");
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry");
+  recommendedAnime = reproduce(recommendedAnime, 4)
+
+  
 
   return (
     <>
       <section>
-      <Header title="paling populer" linkTitle="Lihat semua" linkHref="/populer"/>
-      <Animelist api={topAnime}/>
+        <Header
+          title="paling populer"
+          linkTitle="Lihat semua"
+          linkHref="/populer"
+        />
+        <Animelist api={topAnime} />
+      </section>
+      <section>
+        <Header
+          title="REKOMENDASI"
+        />
+        <Animelist api={recommendedAnime} />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,24 +1,22 @@
-import { getAnimeResponse } from "@/app/libs/api-libs"
-import Animelist from "@/components/Animelist"
-import Header from "@/components/Animelist/Header"
+import { getAnimeResponse } from "@/libs/api-libs";
+import Animelist from "@/components/Animelist";
+import Header from "@/components/Animelist/Header";
 
-const Page = async ({params}) => {
+const Page = async ({ params }) => {
+  const { keyword } = params;
+  const decodedKeyword = decodeURI(keyword);
+  console.log(decodedKeyword);
 
-    const {keyword} = params
-    const decodedKeyword = decodeURI(keyword)
-    console.log(decodedKeyword)
-
-  const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
-    
+  const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`);
 
   return (
     <>
       <section>
-      <Header title={`pencarian untuk ${decodedKeyword}...`} />
-      <Animelist api={searchAnime}/>
+        <Header title={`pencarian untuk ${decodedKeyword}...`} />
+        <Animelist api={searchAnime} />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
