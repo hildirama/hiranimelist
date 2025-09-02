54a8479f3b1a67c4ca0bca3a7536cf36d7972f0f
hildirama <hildirama123@gmail.com>
2025-09-02 19:03:22 +0700

    VALIDATION user dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,13 +1,22 @@
 import {authUserSession} from "@/libs/auth-libs"
 import Image from "next/image"
+import Link from "next/link"
 
 const Page = async() => {
     const user = await authUserSession()
     return (
-        <div className="text-color-primary">
-            <h3>DASHBOARD</h3>
-            <h5>welcome {user.name}</h5>
+        <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">welcome {user.name}</h5>
             <Image src={user.image} alt="..." width={250} height={250}/>
+            <div className="flex flex-wrap gap-4 py-8">
+                <Link href="/users/dashboard/collection" 
+                className="bg-color-accent text-color-dark 
+                font-bold px-4 py-3 text-xl">my collection</Link>
+                
+                <Link href="/users/dashboard/comment" 
+                className="bg-color-accent text-color-dark 
+                font-bold px-4 py-3 text-xl">my comment</Link>
+            </div>
         </div>
     )
 }
