a98f1ec067049b5280c2e6b2e18a4efa04573454
hildirama <hildirama123@gmail.com>
2025-09-03 12:26:56 +0700

    upgrade Next.js v13 to v14

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,17 +1,20 @@
 "use client"
 
 import { FileSearchIcon } from "@phosphor-icons/react"
-import Link from "next/link"
+
+import { useRouter } from "next/navigation"
 
 
 const Page = () => {
+    const router = useRouter()
+
     return (
         <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
             <div className="flex justify-center items-center gap-4 flex-col">
                 <FileSearchIcon size={44} className="text-color-accent"/>
                 <h3 className="text-color-accent text-2xl font-bold">NOT FOUND</h3>
-                <Link href="/" className="text-color-primary hover:text-color-accent
-                transition-all underline">kembali</Link>
+                <button onClick={() => router.back( )} className="text-color-primary hover:text-color-accent
+                transition-all underline">kembali</button>
 
 
             </div>
